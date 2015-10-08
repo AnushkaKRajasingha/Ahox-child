dfd1555d7c92efda23236451b42aafdea827804c
AnushkaKRajasingha <anushkakrajasingha@gmail.com>
2015-10-08 08:41:21 +0530

    re-added missing bootstrap scripts
    re-added missing bootstrap scripts

--- functions.php
+++ functions.php
@@ -2,6 +2,9 @@
 function ahox_childtheme_scripts() {
 	wp_enqueue_style( 'style-ahox_childtheme', get_stylesheet_directory_uri().'/css/style.css' );
 	wp_enqueue_script( 'script-ahox_childtheme',get_stylesheet_directory_uri() . '/js/script.js', array(), '1.0.0', true );
+	
+	wp_enqueue_style( 'style-bootstrap', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' );
+	wp_enqueue_script( 'script-bootstrap','https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js' );
 }
 
 add_action( 'wp_enqueue_scripts', 'ahox_childtheme_scripts' );
